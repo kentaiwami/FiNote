a9e5d8d5989d1711c8d59e0c0fc939cac97d9b60
Kenta Iwami <b1013068@fun.ac.jp>
2016-10-12 16:04:00 +0900

    SNSとの連携処理を記述

--- FiNote/www/js/index.js
+++ FiNote/www/js/index.js
@@ -1578,6 +1578,30 @@ var movieadd = {
         });
 
     },
+
+    /**
+     * Twitter、FaceBook、LINEなどのSNSに投稿する
+     */
+    sns_share: function() {
+        var options = {
+            message: 'share this', // not supported on some apps (Facebook, Instagram)
+            subject: 'the subject', // fi. for email
+            files: ['', ''], // an array of filenames either locally or remotely
+            url: 'https://www.website.com/foo/#bar?a=b',
+            chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
+        };
+
+        var onSuccess = function(result) {
+            console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+            console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+        };
+
+        var onError = function(msg) {
+            console.log("Sharing failed with message: " + msg);
+        };
+
+        window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
+    },
 };
 
 var movieadd_feeling = {
