a9058dc265c022104615d59afe172600e6c5b57f
Kenta Iwami <g2117009@fun.ac.jp>
2017-04-19 15:07:49 +0900

    strを追加

--- myapi/FiNote_API/models.py
+++ myapi/FiNote_API/models.py
@@ -5,13 +5,22 @@ class User(models.Model):
     name = models.CharField(max_length=30, unique=True)
     password = models.CharField(max_length=300)
 
+    def __str__(self):
+        return self.name
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Onomatopoeia(models.Model):
     name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Movie(models.Model):
@@ -21,8 +30,14 @@ class Movie(models.Model):
     user = models.ManyToManyField(User)
     onomatopoeia = models.ManyToManyField(Onomatopoeia)
 
+    def __str__(self):
+        return self.title
+
 
 class OnomatopoeiaCount(models.Model):
     count = models.IntegerField(default=0)
     onomatopoeia = models.ForeignKey(Onomatopoeia)
     movie = models.ForeignKey(Movie)
+
+    def __str__(self):
+        return self.onomatopoeia + '_' + self.count
