d290db9608b3298af325a362c5a8bc90230fe490
Kenta Iwami <g2117009@fun.ac.jp>
2017-05-25 14:42:04 +0900

    映画追加のコマンドを作成した

--- myapi/FiNote_API/management/commands/movie_add.py
+++ myapi/FiNote_API/management/commands/movie_add.py
@@ -2,8 +2,13 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.management import BaseCommand
 from django.db.models import Max, Min
 from rest_framework_jwt.serializers import User
-from FiNote_API.models import Onomatopoeia, Movie
+
+from FiNote_API.functions import MovieAdd, Backup
+from FiNote_API.models import Onomatopoeia
 import random
+import requests
+from myapi.settings import TMDB_APIKEY
+
 
 class Command(BaseCommand):
     help = 'Movie add random choice movies'
@@ -19,19 +24,147 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not options['test']:
-            user = self.choice_onomatopoeia()
+            for i in range(0, 3):
+                params = self.get_params()
+                movie = params['movie']
+
+                if movie == {}:
+                    continue
+
+                # ジャンルの保存
+                genre_obj_dict, genre_obj_list = MovieAdd.genre(self, movie['genre_ids'])
+
+                # 映画の保存
+                data = {'username': params['user'].username,
+                        'movie_title': movie['title'],
+                        'movie_id': movie['tmdb_id'],
+                        'overview': movie['overview'],
+                        'poster_path': movie['poster_path']
+                        }
+                MovieAdd.movie(self, genre_obj_list, params['onomatopoeia'], data)
+
+                # バックアップの保存
+                backup_data = {'username': params['user'].username,
+                               'movie_id': movie['tmdb_id'],
+                               'onomatopoeia_obj_list': params['onomatopoeia'],
+                               'dvd': params['dvd'],
+                               'fav': params['fav']
+                               }
+
+                Backup.movieadd_backup(self, backup_data)
+
+                self.stdout.write(self.style.SUCCESS('***** Movie Add Success *****'))
+                self.output_console(params)
+                self.stdout.write(self.style.SUCCESS('count: ' + str(i)))
+                self.stdout.write(self.style.SUCCESS('***** Movie Add Success *****'))
         else:
-            pass
+            params = self.get_params()
+            movie = params['movie']
 
-        # Top rated movies, Upcoming movies, Now playing movies, Popular movies, random choiceかを、
-        # ルーレットホイールセレクションを使って選択する(最新の映画は割合を多めに)
-        # TMDBのAPIから映画を取得
-        # ランダムで映画を選択
-        # ランダムでDVDとFAVを選択
+            if movie != {}:
+                self.stdout.write(self.style.SUCCESS('***** Test Success *****'))
+                self.output_console(params)
+                self.stdout.write(self.style.SUCCESS('***** Test Success *****'))
 
-        # 映画を登録する
+    def get_movie(self):
+        """
+        Request TMDB Database and get movie information.
+        :return: Movies information(title, overview, tmdb_id, poster_path, genre_ids).
+        """
 
+        language_list = ['ja-JP', 'en-US']
+        api_list = ['upcoming', 'top_rated', 'popular', 'now_playing']
+        random_api_number = random.randint(0, len(api_list)-1)
 
+        # ページ数を把握するためにリクエストを投げる
+        url = 'https://api.themoviedb.org/3/movie/' + api_list[random_api_number]
+        query = {
+            'api_key': TMDB_APIKEY,
+            'language': language_list[0]
+        }
+        tmp_request = requests.get(url, params=query)
+        tmp_request_json = tmp_request.json()
+        total_pages = tmp_request_json['total_pages']
+
+        # ページを決めるためのルーレットホイールセレクションを行う(ページ頭を多めに)
+        rate_list = []
+        for i in range(0, total_pages):
+            if i in range(0, 3):
+                rate_list.append(total_pages * 10)
+            elif i in range(3, 6):
+                rate_list.append((total_pages * 1))
+            else:
+                rate_list.append((total_pages * 0.1))
+
+        arrow = random.randint(1, int(sum(rate_list)))
+        hit_number = 0
+        x = rate_list[hit_number]
+        while arrow > x:
+            hit_number += 1
+            x += rate_list[hit_number]
+
+        # 日本語と英語のリクエストを投げる
+        select_movie_index = 0
+        movie = {}
+        for i, language in enumerate(language_list):
+            url = 'https://api.themoviedb.org/3/movie/' + api_list[random_api_number]
+            query = {
+                'api_key': TMDB_APIKEY,
+                'language': language,
+                'page': hit_number
+            }
+            request = requests.get(url, params=query)
+            request_json = request.json()
+
+            if i == 0:
+                try:
+                    select_movie_index = random.randint(0, len(request_json['results']) - 1)
+                except KeyError:
+                    self.stdout.write(self.style.ERROR('KeyError'))
+                    return {}
+
+            movie = request_json['results'][select_movie_index]
+
+            if movie['overview'] != '':
+                break
+
+        # データの整形
+        if movie['original_language'] == 'ja':
+            if movie['original_title'] != '':
+                title = movie['original_title']
+            else:
+                title = movie['title']
+        else:
+            if movie['title'] != '':
+                title = movie['title']
+            else:
+                title = movie['original_title']
+
+        json_movie = {"title": title,
+                      "overview": movie['overview'],
+                      "tmdb_id": movie['id'],
+                      "poster_path": movie['poster_path'],
+                      "genre_ids": movie['genre_ids']}
+
+        return json_movie
+
+    def get_params(self):
+        """
+        Get params(user, onomatopoeia, dvd_status, fav_status and a movie information).
+        :return: Params. 
+        """
+
+        user = self.choice_user()
+        onomatopoeia = self.choice_onomatopoeia()
+        dvd_status = random.randint(0, 1)
+        fav_status = random.randint(0, 1)
+        movie = self.get_movie()
+
+        return {"user": user,
+                "onomatopoeia": onomatopoeia,
+                "dvd": dvd_status,
+                "fav": fav_status,
+                "movie": movie}
 
     def choice_onomatopoeia(self):
         """
@@ -72,3 +205,25 @@ class Command(BaseCommand):
                 pass
 
         return user
+
+    def output_console(self, param):
+        """
+        Output user params to console.
+        :param param: result params(user, onomatopoeia object list, dvd, fav and movie dictionary).
+
+        :type param: dict
+        """
+
+        onomatopoeia_str = ''
+        for onomatopoeia in param['onomatopoeia']:
+            onomatopoeia_str += onomatopoeia.__str__() + ','
+        onomatopoeia_str = onomatopoeia_str[:-1]
+
+        try:
+            self.stdout.write(self.style.SUCCESS('username: ' + param['user'].username))
+            self.stdout.write(self.style.SUCCESS('onomatopoeia: ' + onomatopoeia_str))
+            self.stdout.write(self.style.SUCCESS('dvd: ' + str(param['dvd'])))
+            self.stdout.write(self.style.SUCCESS('fav: ' + str(param['fav'])))
+            self.stdout.write(self.style.SUCCESS('movie: ' + str(param['movie'])))
+        except UnicodeEncodeError:
+            self.stdout.write(self.style.SUCCESS('Process is success but output UnicodeEncodeError.'))
