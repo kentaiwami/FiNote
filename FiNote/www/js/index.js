0b70e97fff640da5fd91445c3639db23f61e860c
Kenta Iwami <g2117009@fun.ac.jp>
2017-06-06 13:48:20 +0900

    event.target.id if fixed

--- FiNote/www/js/index.js
+++ FiNote/www/js/index.js
@@ -378,7 +378,7 @@ var Top = {
    */
   check_post_change: function(){
     document.addEventListener('postchange', function(event) {
-      if (event.target.id == 'top_carousel') {
+      if (event.target.id === 'top_carousel') {
         console.log('active carousel is ' + event.activeIndex);
 
         var toolbar_center = document.getElementById(ID.get_top_ID().toolbar_center);
