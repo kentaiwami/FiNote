33d4aceb88151771f327d5b90a7bba8a7995f2f9
Kenta Iwami <g2117009@fun.ac.jp>
2017-06-16 14:32:17 +0900

    円グラフのアニメーションを削除

--- FiNote/www/js/index.js
+++ FiNote/www/js/index.js
@@ -3637,41 +3637,6 @@ var User = {
       height: intViewportWidth * 0.45,
       total: total_count
     });
-
-    chart.on('draw', function(data) {
-      if(data.type === 'slice') {
-        var pathLength = data.element._node.getTotalLength();
-
-        data.element.attr({
-          'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
-        });
-
-        var animationDefinition = {
-          'stroke-dashoffset': {
-            id: 'anim' + data.index,
-            dur: 300,
-            from: -pathLength + 'px',
-            to:  '0px',
-            easing: Chartist.Svg.Easing.easeOutQuint,
-            fill: 'freeze'
-          }
-        };
-
-        if(data.index !== 0) {
-          animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
-        }
-
-        data.element.attr({
-          'stroke-dashoffset': -pathLength + 'px'
-        });
-
-        data.element.animate(animationDefinition, false);
-      }
-    });
-
-    chart.on('created', function() {
-      chart.update.bind(chart);
-    });
   }
 };
 
