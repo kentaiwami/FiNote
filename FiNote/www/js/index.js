a6aa2ef0fc99ae41e09737393854eb3064289980
Kenta Iwami <g2117009@fun.ac.jp>
2017-06-27 03:00:11 +0900

    Topを別ファイルにした

--- FiNote/www/js/index.js
+++ FiNote/www/js/index.js
@@ -125,44 +125,6 @@ var Index = {
 
 
 
-/************************************************************
-                        top.html
- ************************************************************/
-var Top = {
-  /**
-   * カルーセルの変更イベントをキャッチして、ツールバーのメッセージを変更する関数
-   */
-  check_post_change: function(){
-    document.addEventListener('postchange', function(event) {
-      if (event.target.id === 'top_carousel') {
-        console.log('active carousel is ' + event.activeIndex);
-
-        var toolbar_center = document.getElementById(ID.get_top_ID().toolbar_center);
-        if (event.activeIndex === 0) {
-          toolbar_center.innerHTML = 'ユーザ登録';
-        }else {
-          toolbar_center.innerHTML = 'ログイン';
-        }
-      }
-    });
-  },
-
-
-  prev: function() {
-    var carousel = document.getElementById(ID.get_top_ID().carousel);
-    carousel.prev();
-  },
-
-
-  next: function() {
-    var carousel = document.getElementById(ID.get_top_ID().carousel);
-    carousel.next();
-  }
-};
-
-
-
-
 
 /************************************************************
                          signup.html
