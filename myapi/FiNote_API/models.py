808ecaac6c504b05348a413facde2d7e98d2e8df
Kenta Iwami <g2117009@fun.ac.jp>
2017-06-30 01:15:59 +0900

    migrateの削除、改行追加

--- myapi/FiNote_API/models.py
+++ myapi/FiNote_API/models.py
@@ -7,6 +7,7 @@ from rest_framework.authtoken.models import Token
 from myapi import settings
 import datetime
 
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
