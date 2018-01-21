53ab4f45a882d6039a677b36b37a51b291ed03d9
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-22 07:02:22 +0900

    moviesのファイル作成

---
+++ ios/FiNote/FiNote/Movie/MoviesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesViewController.swift
+//  FiNote
+//
+//  Created by 岩見建汰 on 2018/01/22.
+//  Copyright © 2018年 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
