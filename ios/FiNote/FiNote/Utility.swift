2410c9e037d28236ae5e45c5d34401cab7933a73
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-21 17:32:36 +0900

    add liblary, ios標準のアラートを出す関数を削除

--- ios/FiNote/FiNote/Utility.swift
+++ ios/FiNote/FiNote/Utility.swift
@@ -8,31 +8,6 @@
 
 import UIKit
 
-func GetStandardAlert(title: String, message: String, b_title: String) -> UIAlertController {
-    let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
-    let ok = UIAlertAction(title: b_title, style:UIAlertActionStyle.default)
-    
-    alertController.addAction(ok)
-    
-    return alertController
-}
-
-func GetDestructiveCancelAlert(title: String, message: String, button_title: String, destructive_action: @escaping () -> Void) -> UIAlertController {
-    let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
-    
-    let destructive = UIAlertAction(title: button_title, style: UIAlertActionStyle.destructive, handler:{(action: UIAlertAction!) -> Void in
-        destructive_action()
-    })
-    let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel)
-    
-    alertController.addAction(destructive)
-    alertController.addAction(cancel)
-    
-    return alertController
-}
-
-
 func GetAppDelegate() -> AppDelegate {
     return UIApplication.shared.delegate as! AppDelegate
 }
-
