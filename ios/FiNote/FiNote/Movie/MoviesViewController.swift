e385af197c1e7dd3a2ba03052ce1671dfa5a04c3
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-22 07:07:22 +0900

    add test code

--- ios/FiNote/FiNote/Movie/MoviesViewController.swift
+++ ios/FiNote/FiNote/Movie/MoviesViewController.swift
@@ -8,13 +8,75 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let myTableView = UITableView(frame: view.frame, style: .plain)
+        myTableView.rowHeight = 70
+        myTableView.delegate      =   self
+        myTableView.dataSource    =   self
+        myTableView.register(MyCell.self, forCellReuseIdentifier: NSStringFromClass(MyCell.self))
+        self.view.addSubview(myTableView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(MyCell.self), for: indexPath) as! MyCell
+        cell.myLabel.text = "まぐろ"
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Sample"
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("HOGEOHGE:", indexPath)
+    }
+}
+
+class MyCell: UITableViewCell {
+    var myLabel: UILabel!
+    var myImage: UIImage!
+    var myImageView: UIImageView!
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        myLabel = UILabel(frame: CGRect.zero)
+        myLabel.textAlignment = .left
+        contentView.addSubview(myLabel)
+        
+        myImage = UIImage(named: "maguro.jpg")
+        myImageView = UIImageView(image: myImage)
+        contentView.addSubview(myImageView)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder: ) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        myLabel.frame = CGRect(x: 110, y: 0, width: frame.width - 100, height: frame.height)
+        myImageView.frame = CGRect(x: 0, y: 0, width: 100, height: frame.height)
+    }
+    
 }
