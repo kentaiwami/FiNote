4bfd81dc75dc4669b1ac34f2eff21e43cf7bdf9e
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-22 01:15:08 +0900

    add icons

--- ios/FiNote/FiNote/Sign/SignUpViewController.swift
+++ ios/FiNote/FiNote/Sign/SignUpViewController.swift
@@ -12,8 +12,6 @@ class SignUpViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        self.view.backgroundColor = UIColor.clear
         self.CreateForm()
     }
     
