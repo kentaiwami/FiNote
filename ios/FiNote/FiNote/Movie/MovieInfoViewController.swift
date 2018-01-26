961ab4fda68a2e39b7bfca84d0abb005716b4211
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-26 16:35:07 +0900

    オノマトペとdvd,fav編集画面のファイルを作成

---
+++ ios/FiNote/FiNote/Movie/MovieInfoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MovieInfoViewController.swift
+//  FiNote
+//
+//  Created by 岩見建汰 on 2018/01/26.
+//  Copyright © 2018年 Kenta. All rights reserved.
+//
+
+import UIKit
+import Eureka
+
+class MovieInfoViewController: FormViewController {
+
+    private var onomatopoeia: [String] = []
+    private var dvd = false
+    private var fav = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func SetOnomatopoeia(onomatopoeia: [String]) {
+        self.onomatopoeia = onomatopoeia
+    }
+    
+    func SetDVD(dvd: Bool) {
+        self.dvd = dvd
+    }
+    
+    func SetFAV(fav: Bool) {
+        self.fav = fav
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
