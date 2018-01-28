3bc96410b5ba9ab5b417b40881e374d96f8b0088
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-28 22:00:44 +0900

    パスワードのformを生成

--- ios/FiNote/FiNote/User/UserDetailFormViewController.swift
+++ ios/FiNote/FiNote/User/UserDetailFormViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import Eureka
+import KeychainAccess
 
-class UserDetailFormViewController: UIViewController {
+class UserDetailFormViewController: FormViewController {
 
     var api_name = ""
+    var screen_title = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        self.navigationItem.title = "Edit Info"
+        Create()
+        
+        self.navigationItem.title = screen_title
         let check = UIBarButtonItem(image: UIImage(named: "icon_check"), style: .plain, target: self, action: #selector(TapCheckButton))
         self.navigationItem.setRightBarButton(check, animated: true)
     }
@@ -28,16 +37,44 @@ class UserDetailFormViewController: UIViewController {
         
     }
     
-    func CreateForm() {
+    func Create() {
+        UIView.setAnimationsEnabled(false)
+        form.removeAll()
         
+        let section = Section("")
+        
+        switch api_name {
+        case "password":
+            screen_title = "Edit Password"
+            section.append(CreatePassWordRow(title: "現在のパスワード", tag: "now_pass"))
+            section.append(CreatePassWordRow(title: "新しいパスワード", tag: "new_pass"))
+        case "email":
+            screen_title = "Edit Email"
+        case "birthyear":
+            screen_title = "Edit birthyear"
+        default:
+            screen_title = ""
+            break
+        }
+        
+        form.append(section)
+        
+        UIView.setAnimationsEnabled(true)
     }
     
     func SetAPIName(name: String) {
         api_name = name
     }
     
-    func CreatePassWordRow() {
+    func CreatePassWordRow(title: String, tag: String) -> PasswordRow {
+        let row = PasswordRow()
+        row.title = title
+        row.value = ""
+        row.add(rule: RuleRequired(msg: "必須項目です"))
+        row.validationOptions = .validatesOnChange
+        row.tag = tag
         
+        return row
     }
     
     func CreateTextRow() {
