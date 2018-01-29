5b367517b4b8f95b46cec587c8f7ae0e9f2c65b5
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-29 19:14:23 +0900

    controller,cellのファイル生成

---
+++ ios/FiNote/FiNote/Social/SocialByAgeCell.swift
@@ -0,0 +1,29 @@
+//
+//  SocialByAgeCell.swift
+//  FiNote
+//
+//  Created by 岩見建汰 on 2018/01/29.
+//  Copyright © 2018年 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class SocialByAgeCell: UICollectionViewCell {
+    var textLabel : UILabel?
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // UILabelを生成.
+        textLabel = UILabel(frame: CGRect(x:0, y:0, width:frame.width, height:frame.height))
+        textLabel?.text = "nil"
+        textLabel?.textAlignment = NSTextAlignment.center
+        
+        // Cellに追加.
+        self.contentView.addSubview(textLabel!)
+    }
+}
--- ios/FiNote/FiNote/Social/SocialByAgeViewController.swift
+++ ios/FiNote/FiNote/Social/SocialByAgeViewController.swift
@@ -8,12 +8,61 @@
 
 import UIKit
 
-class SocialByAgeViewController: UIViewController {
-
+class SocialByAgeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor.brown
+    }
+    
+    func InitCollectionView() {
+        let w = 50 as CGFloat
+        let h = w*1.0 as CGFloat
+        let margin = (self.view.frame.width - w*2) / 4
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: w, height: h)
+        layout.minimumInteritemSpacing = margin
+        layout.minimumLineSpacing = margin
+        layout.sectionInset = UIEdgeInsets(top: margin, left: margin, bottom: margin, right: margin)
+        layout.headerReferenceSize =  CGSize(width:100,height:50)
+        
+        let collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView.register(SocialByAgeCell.self, forCellWithReuseIdentifier: "MyCell")
+        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "Section")
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        self.view.addSubview(collectionView)
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Num: \(indexPath.row)")
+        print("SectionNum:\(indexPath.section)")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "Section", for: indexPath)
+        
+        headerView.backgroundColor = UIColor.white
+        
+        return headerView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell : SocialByAgeCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath as IndexPath) as! SocialByAgeCell
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
