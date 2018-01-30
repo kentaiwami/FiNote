170480a77b171dd4c5841e445b12bee1d87946aa
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-30 11:13:15 +0900

    パース処理を修正、トップへスクロールを実装

--- ios/FiNote/FiNote/Social/SocialContainViewController.swift
+++ ios/FiNote/FiNote/Social/SocialContainViewController.swift
@@ -12,9 +12,12 @@ import Alamofire
 import NVActivityIndicatorView
 import SwiftyJSON
 
-class SocialContainViewController: UIViewController, UISearchBarDelegate, StatusController {
-
+class SocialContainViewController: UIViewController, UISearchBarDelegate, StatusController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UITabBarControllerDelegate {
+    
     let searchBar = UISearchBar()
+    let cellId = "MyCell"
+    var preViewName = "Social"
+    var collectionView = UICollectionView(frame: CGRect(), collectionViewLayout: UICollectionViewLayout())
     var movies: [MovieBasic.Data] = []
     
     override func viewDidLoad() {
@@ -32,6 +35,7 @@ class SocialContainViewController: UIViewController, UISearchBarDelegate, Status
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.tabBarController?.navigationItem.titleView = searchBar
+        self.tabBarController?.delegate = self
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -67,9 +71,12 @@ class SocialContainViewController: UIViewController, UISearchBarDelegate, Status
                 print("***** API results *****")
                 
                 if IsHTTPStatus(statusCode: response.response?.statusCode) {
-                    for data in obj.arrayValue {
+                    self.movies.removeAll()
+                    
+                    for data in obj["results"].arrayValue {
                         self.movies.append(MovieBasic().GetData(json: data))
                     }
+                    
                     self.DrawView()
                 }else {
                     ShowStandardAlert(title: "Error", msg: obj.arrayValue[0].stringValue, vc: self)
@@ -79,12 +86,74 @@ class SocialContainViewController: UIViewController, UISearchBarDelegate, Status
     }
     
     func DrawView() {
-        //TODO: Viewの描画
+        collectionView.removeFromSuperview()
+        
         if movies.count == 0 {
             let status = Status(title: "No Results", description: "指定したオノマトペを含む映画は見つかりませんでした", actionTitle: "", image: nil)
             show(status: status)
         }else {
+            InitScrollView()
         }
+    }
+    
+    func InitScrollView() {
+        let w = 150.0 as CGFloat
+        let h = w*1.5 as CGFloat
+        let margin = (self.view.frame.width - w*2) / 4
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: w, height: h)
+        layout.minimumInteritemSpacing = margin
+        layout.minimumLineSpacing = margin
+        layout.sectionInset = UIEdgeInsets(top: margin, left: margin, bottom: margin, right: margin)
+        
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height), collectionViewLayout: layout)
+        collectionView.backgroundColor = UIColor.white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.autoresizingMask = UIViewAutoresizing(rawValue: UIViewAutoresizing.RawValue(UInt8(UIViewAutoresizing.flexibleHeight.rawValue) | UInt8(UIViewAutoresizing.flexibleWidth.rawValue)))
+        self.view.addSubview(collectionView)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        for subview in cell.contentView.subviews {
+            subview.removeFromSuperview()
+        }
+        
+        let urlRequest = URL(string: API.poster_base.rawValue+movies[indexPath.row].poster)!
+        let poster = UIImageView()
+        poster.af_setImage(
+            withURL: urlRequest,
+            placeholderImage: UIImage(named: "no_image")
+        )
+        poster.frame = CGRect(x: 0, y: 0, width: cell.contentView.frame.width, height: cell.contentView.frame.height)
+        poster.layer.shadowOpacity = 0.5
+        poster.layer.shadowColor = UIColor.black.cgColor
+        poster.layer.shadowOffset = CGSize(width: 1, height: 1)
+        poster.layer.shadowRadius = 3
+        poster.layer.masksToBounds = false
+        cell.contentView.addSubview(poster)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let title = movies[indexPath.row].title
+        ShowStandardAlert(title: title, msg: movies[indexPath.row].overview, vc: self)
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        if viewController.restorationIdentifier! == "Social" && preViewName == "Social" {
+            collectionView.scroll(to: .top, animated: true)
+        }
+        preViewName = "Social"
     }
 
     override func didReceiveMemoryWarning() {
