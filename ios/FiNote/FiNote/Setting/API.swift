ce49e6119f243be127856020ae8ec081524e9d5d
Kenta Iwami <g2117009@fun.ac.jp>
2018-04-25 21:36:32 +0900

    Eurekaのアップデート、API URLの更新

--- ios/FiNote/FiNote/Setting/API.swift
+++ ios/FiNote/FiNote/Setting/API.swift
@@ -7,7 +7,7 @@
 //
 
 enum API: String {
-    case base = "https://kentaiwami.jp/FiNote/django.cgi/api/"
+    case base = "https://kentaiwami.jp/finote/api/"
     case v1 = "v1/"
     case user = "user/"
     case signup = "signup/"
