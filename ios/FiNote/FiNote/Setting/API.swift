a05409ad163ad781ff172267ed135e928c7646f6
Kenta Iwami <g2117009@fun.ac.jp>
2018-12-12 17:51:57 +0900

    update

--- ios/FiNote/FiNote/Setting/API.swift
+++ ios/FiNote/FiNote/Setting/API.swift
@@ -7,7 +7,7 @@
 //
 
 enum API: String {
-    case base = "https://finote.kentaiwami.jp/api/"
+    case base = "https://finote.kentaiwami.com/api/"
     case v1 = "v1/"
     case user = "user/"
     case signup = "signup/"
